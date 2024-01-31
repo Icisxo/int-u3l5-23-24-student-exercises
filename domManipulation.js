65ae6e24ef00ea96ff2b627d0ea7992969b6ec45
Icisxo <146958163+Icisxo@users.noreply.github.com>
2024-01-31 14:54:28 +0000

    style

--- domManipulation.js
+++ domManipulation.js
@@ -10,17 +10,30 @@ let row1 = document.querySelector(".row-1");
 
 // 1. Set the styling of `example1` to a background color of black.
 
+example1.style.backgroundColor="black";
+
+
 // 2. Set the styling of `pink` to a background color of pink.
+
+pink.style.backgroundColor="pink";
+
 
 // 3. Set the styling of `green` to a background color of green.
 
+green.style.backgroundColor="green";
+
+
 // 4. Set the styling of `blue` to a background color of blue.
+
+blue.style.backgroundColor="blue";
 
 // 5. Set the styling of `lightGreen` to a background color of lightGreen.
 
+lightGreen.style.backgroundColor="lightGreen";
+
 // 6. Set the styling of `row1` to have a font color of white.
 
-
+row1.style.color="white";
 
 // CHALLENGE #2 - style the image
 
@@ -29,6 +42,10 @@ let imageButton = document.querySelector(".image-button");
                                    
 // 7. Add an event listener to `imageButton`
 // - The event type should be "click" or "dblclick"
+
+
+imageButton.onclick("click", function (){image.stylewidth="300px";}
+
 
 // Include the next tasks in the body of the function.
 
